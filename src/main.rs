f9b691b68007d9e03b6769b000ac221c7b707084
Aidan Smith <asmith2@scu.edu>
2023-02-28 11:06:09 -0800

    🏥 Health Routes and Tests - Added healthCheck route and 404 response fallback. - Added tests of /get and /update - Updated update to respond with OK and not ACCEPTED.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ mod filters {
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         post_update(db.clone())
         .or(get_data(db.clone()))
+        .or(health_check())
+        .or(not_found())
     }
 
     pub fn post_update(
@@ -43,6 +45,18 @@ mod filters {
             .and(warp::get())
             .and(with_db(db))
             .and_then(handlers::get)
+    }
+
+    pub fn health_check() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("healthCheck")
+            .and(warp::get())
+            .map(|| warp::reply::with_status("OK", warp::http::StatusCode::OK))
+    }
+
+    pub fn not_found() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::any()
+            .and(warp::path::end())
+            .map(|| warp::reply::with_status("Not Found", warp::http::StatusCode::NOT_FOUND))
     }
 
     fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
@@ -85,7 +99,7 @@ mod handlers {
 
         Ok(warp::reply::with_status(
             "Fetching update.",
-            warp::http::StatusCode::ACCEPTED,
+            warp::http::StatusCode::OK,
         ))
     }
 
@@ -95,81 +109,7 @@ mod handlers {
         Ok(warp::reply::json(&resp["items"]))
     }
 
-    // pub async fn render_response(db: Db) -> Result<impl warp::Reply, warp::Rejection> {
-    //     let resp = get_spins(db).await;
-    //     match resp {
-    //         Ok(_) => Ok(warp::reply::with_status(
-    //             "Fetching update.",
-    //             warp::http::StatusCode::PROCESSING,
-    //         )),
-    //         Err(_) => Ok(warp::reply::with_status(
-    //             "Failed.",
-    //             warp::http::StatusCode::FORBIDDEN,
-    //         )),
-    //     }
-    // }
 }
-
-// async fn hello(
-//     param: Option<HashMap<String, String>>,
-// ) -> Result<impl warp::Reply, warp::Rejection> {
-//     // thread::spawn(|| {
-//     //     get_spins();
-//     // }).join().expect("Thread panicked");
-
-//     // Unpack hashmap
-//     match param {
-//         Some(param) => {
-//             let song_title = param.get("title").unwrap();
-//             let artist = param.get("artist").unwrap();
-//             println!("song_title: {}, artist: {}", song_title, artist);
-//             Ok(warp::reply::with_status(
-//                 "Successfully updated current song.",
-//                 warp::http::StatusCode::CREATED,
-//             ))
-//         }
-//         None => {
-//             println!("No params");
-//             Ok(warp::reply::with_status(
-//                 "No params",
-//                 warp::http::StatusCode::CREATED,
-//             ))
-//         }
-//     }
-// }
-
-// fn parse_body(mut body: impl Buf) -> Option<HashMap<String, String>> {
-//     if body.remaining() < MIN_LEN {
-//         return None;
-//     }
-
-//     // Declare hashmap
-//     let mut map: HashMap<String, String> = HashMap::new();
-
-//     let mut peek_buffer = vec![0; std::cmp::min(body.remaining(), MAX_LEN)];
-//     body.copy_to_slice(&mut peek_buffer);
-
-//     let parts: Vec<&str> = std::str::from_utf8(&peek_buffer)
-//         .ok()?
-//         .split(|c| c == '=' || c == '&')
-//         .collect();
-
-//     println!("parts: {:?}", parts);
-
-//     // Iterate over parts and add to hashmap
-//     for i in 0..parts.len() {
-//         if i + 1 >= parts.len() {
-//             break;
-//         }
-//         if i % 2 == 0 {
-//             map.insert(parts[i].to_string(), parts[i + 1].to_string());
-//         }
-//     }
-
-//     println!("map: {:?}", map);
-//     // Return hashmap
-//     return Some(map.clone());
-// }
 
 mod models {
     use serde_json::{json, Value};
@@ -181,4 +121,74 @@ mod models {
     pub fn blank_db() -> Db {
         Arc::new(Mutex::new(json!(null)))
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use warp::http::StatusCode;
+    use warp::test::request;
+
+    use super::{
+        filters,
+        models::{self, Db},
+    };
+
+    #[tokio::test]
+    async fn test_update() {
+        let db = models::blank_db();
+        let api = filters::routes(db.clone());
+
+        let resp = request()
+            .method("POST")
+            .path("/update")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_get() {
+        let db = models::blank_db();
+        let api = filters::routes(db.clone());
+
+        let resp = request()
+            .method("GET")
+            .path("/get")
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_health_check() {
+        let db = models::blank_db();
+        let api = filters::routes(db.clone());
+
+        let resp = request()
+            .method("GET")
+            .path("/healthCheck")
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_not_found() {
+        let db = models::blank_db();
+        let api = filters::routes(db.clone());
+
+        let resp = request()
+            .method("GET")
+            .path("/not-found")
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), StatusCode::NOT_FOUND);
+    }
+
+
 }
